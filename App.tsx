4b9ce1525a4637ab204b2d0288b2d6646950d2be
Luciano dii Souza <lucianodiisouza@hotmail.com>
2023-11-13 00:17:29 -0300

    wip: product card

--- App.tsx
+++ App.tsx
@@ -3,13 +3,16 @@ import { useEffect, useState } from 'react'
 import {
   ActivityIndicator,
   FlatList,
+  Image,
+  SafeAreaView,
   StyleSheet,
   Text,
   View,
 } from 'react-native'
+import { IProductData } from './types'
 
 export default function App() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState<IProductData[]>([])
   const [isLoading, setIsLoading] = useState(false)
 
   const fetchData = () => {
@@ -28,16 +31,26 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      {isLoading && <ActivityIndicator />}
-      <FlatList
-        data={data}
-        renderItem={({ item }) => (
-          <View>
-            <Text>{JSON.stringify(item)}</Text>
-          </View>
-        )}
-        keyExtractor={(item) => item.id}
-      />
+      <SafeAreaView style={styles.safeArea}>
+        {isLoading && <ActivityIndicator />}
+        <FlatList
+          data={data}
+          style={styles.flatlist}
+          renderItem={({ item }) => (
+            <View style={styles.flatListContainer}>
+              <View>
+                <Text>{item.name}</Text>
+                <Text numberOfLines={2} ellipsizeMode="tail">
+                  {item.description}
+                </Text>
+                <Text>${item.price}</Text>
+              </View>
+              <Image source={{ uri: item.image }} width={80} height={80} />
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
     </View>
   )
 }
@@ -46,7 +59,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  flatListContainer: {
+    flexDirection: 'row',
+    height: 100,
+    width: '98%',
+    backgroundColor: '#efefef',
+    marginVertical: 5,
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    justifyContent: 'space-between',
+  },
+  safeArea: {
+    flex: 1,
+  },
+  flatlist: {
+    flex: 1,
+    paddingHorizontal: 5,
   },
 })
---
+++ types/index.ts
@@ -0,0 +1,7 @@
+export interface IProductData {
+  id: string
+  name: string
+  description: string
+  image: string
+  price: string
+}
